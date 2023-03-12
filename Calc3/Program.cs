07eca22d75bfa6323d37a58397c1fba3b98155ec
Klara Maxova <50149154+klaramax@users.noreply.github.com>
2023-03-12 19:51:36 +0100

    Add files via upload

---
+++ Calc3/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc3
+{
+    internal class Program
+    {
+        static double NactiCislo()
+        {
+            Console.WriteLine("Zadej číslo:");
+            double cislo;
+            while (!double.TryParse(Console.ReadLine(), out cislo))
+            {
+                Console.WriteLine("Špatně zadané číslo. Zadej znovu číslo:");
+            }
+            return cislo;
+        }
+
+        static string NactiOperator()
+        {
+            string[] vsechnyOperace = { "+", "-", "*", "/", "^", "c", "q" };
+            Console.WriteLine("Zvol jeden z operátorů: + - * / ^, c pro vynulování nebo q pro ukončení");
+            string operace = Console.ReadLine();
+
+            while (!vsechnyOperace.Contains(operace))
+            {
+                Console.WriteLine("Špatně zadaný operátor. Zvol: + - * / ^, c pro vynulování nebo q pro ukončení");
+                operace = Console.ReadLine();
+            }
+            return operace;
+        }
+
+        static void Main(string[] args)
+        {
+            double cislo1 = 0;
+            double cislo2 = 0;
+            string operace = "";
+            double vysledek = 0;
+            string zadano = "";
+
+            while (true)
+            { 
+                //Načtení prvního čísla
+                cislo1 = NactiCislo();
+
+                while(true)
+                {
+                    //Načtení operátoru
+                    operace = NactiOperator();
+ 
+                    if (operace == "c")
+                    {
+                        Console.WriteLine("Vynulováno");
+                        break;
+                    }
+
+                    if (operace == "q")
+                    {
+                        return;
+                    }
+
+                    //Načtení dalšího čísla
+                    cislo2 = NactiCislo();
+
+                    // Výpočet a výpis výsledku
+                    if (cislo2 == 0 && operace == "/")
+                    {
+                        Console.WriteLine("Dělení nulou není možné. Začni znovu.");
+                        break;
+                    }
+                    else
+                    {
+                        vysledek = Vypocet.Vypocitej(cislo1, operace, cislo2);
+                        zadano = cislo1.ToString() + " " + operace + " " + cislo2.ToString();
+                        Console.WriteLine($"Výpočet: {zadano} = {vysledek}");
+                        cislo1 = vysledek;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Calc3/Vypocet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc3
+{
+    internal class Vypocet
+    {
+
+        public static double Vypocitej(double cislo1, string operace, double cislo2)
+        {
+            switch (operace)
+            {
+                case "+":
+                    return Secti(cislo1, cislo2);
+                case "-":
+                    return Odecti(cislo1, cislo2);
+                case "*":
+                    return Vynasob(cislo1, cislo2);
+                case "/":
+                    return Vydel(cislo1, cislo2);
+                default:
+                    return Math.Pow(cislo1, cislo2);
+            }
+        }
+
+        public static double Secti(double cislo1, double cislo2)
+        {
+            return cislo1 + cislo2;
+        }
+
+        static double Odecti(double cislo1, double cislo2)
+        {
+            return cislo1 - cislo2;
+        }
+
+        static double Vynasob(double cislo1, double cislo2)
+        {
+            return cislo1 * cislo2;
+        }
+
+        static double Vydel(double cislo1, double cislo2)
+        {
+            return cislo1 / cislo2;
+        }
+    }
+}
